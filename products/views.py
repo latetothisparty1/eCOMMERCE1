065f26ac72584f3dbcf2b00834819eed86d1a8f1
Akram Mathu <mathuakram@gmail.com>
2024-12-10 11:05:15 +0300

    001

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
